e2c49e6f36c90e10d93b031aba0dc6cf94761f63
ellalamont <elamont@uw.edu>
2025-09-11 08:20:46 -0700

    Added PCA graph with just the pH7 Rv (not Ra)

--- PCA.R
+++ PCA.R
@@ -246,6 +246,72 @@ PCA_tpm_1
 #        path = "Figures_preNonCodingRemoval/PCA/wRv",
 #        width = 8, height = 5, units = "in")
 
+###########################################################
+####################### PCA with Rv #######################
+# 9/11/25: Just the pH7 Rv, not Ra
+
+my_fav_colors3 <- c(`Sputum L4` = "#004C73", `Sputum L2` = "#99CCE8", `Caseum mimic` = "green4", `Broth`= "#999999", `Marmoset` = "#6A3D9A", `Rabbit` = "#E69F00", `Rv7` = "#FC8D59", `Rv8.3` = "#FEE090", `Rv5.7` = "#D73027", `IndigoRv` = "black", `NoDrugRv` = "#E7298A") 
+# Labelled Shapes
+my_fav_shapes3 <- c(`Sputum L4` = 21, `Sputum L2` = 21, `Caseum mimic` = 22, `Broth`= 23, `Marmoset` = 24, `Rabbit` = 25, `Rv7` = 23, `Rv8.3` = 23, `Rv5.7` = 23, `IndigoRv` = 23, `NoDrugRv` = 23)
+
+# Passing filter is >1,000,000 genomic reads and >80% genes with at least 10 reads, already subsetted in Import_data.R
+
+# Convert gene column to rownames
+my_tpm <- GoodBiolSamples_w_Rv_tpm %>% column_to_rownames(var = "X")
+
+# Remove the Ra, and others
+# my_tpm <- my_tpm %>% select(-contains("BQ"), -contains("HN878"), -contains("Cav"))
+
+# Transform the data
+my_tpm_t <- as.data.frame(t(my_tpm))
+
+# Remove columns that are all zero so the scale works for prcomp
+my_tpm_t2 <- my_tpm_t %>% select_if(colSums(.) != 0)
+
+# Remove the Ra and other pH Rvs
+my_tpm_t2_subset <- my_tpm_t2 %>% filter(!rownames(my_tpm_t2) %in% c("H37Ra_Broth_4_S7", "H37Ra_Broth_5_S8", "H37Ra_Broth_6_S9", "Rv_pH_8.3_R1", "Rv_pH_8.3_R2", "Rv_pH_5.7_R1", "Rv_pH_5.7_R2", "INDIGO.NoDrug.Control.E1"))
+
+# Make the actual PCA
+my_PCA <- prcomp(my_tpm_t2_subset, scale = TRUE)
+
+# See the % Variance explained
+summary(my_PCA)
+summary_PCA <- format(round(as.data.frame(summary(my_PCA)[["importance"]]['Proportion of Variance',]) * 100, digits = 1), nsmall = 1) # format and round used to control the digits after the decimal place
+summary_PCA[1,1] # PC1 explains 33.9% of variance
+summary_PCA[2,1] # PC2 explains 13.3% of variance
+summary_PCA[3,1] # PC3 explains 9.8% of variance
+
+# MAKE PCA PLOT with GGPLOT 
+my_PCA_df <- as.data.frame(my_PCA$x[, 1:3]) # Extract the first 3 PCs
+my_PCA_df <- data.frame(SampleID = row.names(my_PCA_df), my_PCA_df)
+my_PCA_df <- merge(my_PCA_df, BiolSamples_pipeSummary_2, by = "SampleID", )
+
+PCA_tpm_1 <- my_PCA_df %>% 
+  ggplot(aes(x = PC1, y = PC2, fill = Type3, shape = Type3)) + 
+  geom_point(aes(fill = Type3, shape = Type3), size = 5, alpha = 0.7, stroke = 0.8) +
+  # geom_text_repel(aes(label = SampleID), size = 2.5) + 
+  scale_fill_manual(values = my_fav_colors3) +  
+  scale_shape_manual(values = my_fav_shapes3) + 
+  # geom_text_repel(aes(label = Week), size= 2.5, box.padding = 0.4, segment.color = NA, max.overlaps = Inf) + 
+  labs(title = "PCA: >1M reads and >80% genes with at least 10 reads",
+       subtitle = "TPM",
+       x = paste0("PC1: ", summary_PCA[1,1], "%"),
+       y = paste0("PC2: ", summary_PCA[2,1], "%")) +
+  my_plot_themes
+PCA_tpm_1
+# ggsave(PCA_tpm_1,
+#        file = paste0("TPM_wRv_2.pdf"),
+#        path = "Figures_preNonCodingRemoval/PCA/wRv",
+#        width = 8, height = 5, units = "in")
+
+
+
+# Get the top Gene contributors for the PCA (positive or negative values)
+top_genes_PC2 <- as.data.frame(my_PCA$rotation) %>%
+  rownames_to_column("Gene") %>%
+  arrange(desc(abs(PC2))) %>%
+  slice(1:20) %>%
+  select(Gene, PC2)
 
 
 ###########################################################
