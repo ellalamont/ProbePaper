959aea7b739f31d6334b078baa6ad46bad429df0
ellalamont <elamont@uw.edu>
2025-09-11 08:20:02 -0700

    Added batch corrected PCA with just the pH7 Rv

--- BatchCorrection_PCA.R
+++ BatchCorrection_PCA.R
@@ -210,3 +210,67 @@ PCA_cpm <- my_PCA_cpm_df %>%
   my_plot_themes
 PCA_cpm
 # Doesn't look like CPM is causing much difference, so what is going on with the TPM converted batch corrected samples???
+
+
+###########################################################
+##################### PCA w TPM no RA #####################
+# 9/11/25
+
+my_fav_colors3 <- c(`Sputum L4` = "#004C73", `Sputum L2` = "#99CCE8", `Caseum mimic` = "green4", `Broth`= "#999999", `Marmoset` = "#6A3D9A", `Rabbit` = "#E69F00", `Rv7` = "#FC8D59", `Rv8.3` = "#FEE090", `Rv5.7` = "#D73027") 
+# Labelled Shapes
+my_fav_shapes3 <- c(`Sputum L4` = 21, `Sputum L2` = 21, `Caseum mimic` = 22, `Broth`= 23, `Marmoset` = 24, `Rabbit` = 25, `Rv7` = 23, `Rv8.3` = 23, `Rv5.7` = 23)
+
+# Convert the batch corrected counts to counts per million (cpm)
+counts_corrected_df <- as.data.frame(counts_corrected) %>% rownames_to_column("X")
+my_tpm_bc <- CalculateTPM(counts_corrected_df)
+
+# transform the data 
+my_tpm_bc_t <- as.data.frame(t(my_tpm_bc))
+
+# Remove columns that are all zero so the scale works for prcomp
+my_tpm_bc_t <- my_tpm_bc_t %>% select_if(colSums(.) != 0) # Down to 4494 genes 
+
+# Remove the Ra and other pH Rvs
+my_tpm_bc_t_subset <- my_tpm_bc_t %>% filter(!rownames(my_tpm_bc_t) %in% c("H37Ra_Broth_4_S7", "H37Ra_Broth_5_S8", "H37Ra_Broth_6_S9", "Rv_pH_8.3_R1", "Rv_pH_8.3_R2", "Rv_pH_5.7_R1", "Rv_pH_5.7_R2"))
+
+# Make the actual PCA
+my_PCA_tpm_bc <- prcomp(my_tpm_bc_t_subset, scale = TRUE)
+
+# See the % Variance explained
+summary(my_PCA_tpm_bc)
+summary_PCA_tpm_bc <- format(round(as.data.frame(summary(my_PCA_tpm_bc)[["importance"]]['Proportion of Variance',]) * 100, digits = 1), nsmall = 1) # format and round used to control the digits after the decimal place
+summary_PCA_tpm_bc[1,1] # PC1 explains 35.6% of variance
+summary_PCA_tpm_bc[2,1] # PC2 explains 13.6% of variance
+summary_PCA_tpm_bc[3,1] # PC3 explains 10.5% of variance
+
+# MAKE PCA PLOT with GGPLOT 
+my_PCA_tpm_bc_df <- as.data.frame(my_PCA_tpm_bc$x[, 1:3]) # Extract the first 3 PCs
+my_PCA_tpm_bc_df <- data.frame(SampleID = row.names(my_PCA_tpm_bc_df), my_PCA_tpm_bc_df)
+my_PCA_tpm_bc_df <- merge(my_PCA_tpm_bc_df, BiolSamples_pipeSummary_2, by = "SampleID", )
+
+PCA_BatchCorrected <- my_PCA_tpm_bc_df %>% 
+  ggplot(aes(x = PC1, y = PC2, fill = Type3, shape = Type3)) + 
+  geom_point(aes(fill = Type3, shape = Type3), size = 5, alpha = 0.7, stroke = 0.8) +
+  # geom_text_repel(aes(label = SampleID), size = 2.5) + 
+  scale_fill_manual(values = my_fav_colors3) +  
+  scale_shape_manual(values = my_fav_shapes3) + 
+  # geom_text_repel(aes(label = Week), size= 2.5, box.padding = 0.4, segment.color = NA, max.overlaps = Inf) + 
+  labs(title = "PCA: >1M reads and >80% genes with at least 10 reads",
+       subtitle = "Batch Corrected Converted to TPM",
+       x = paste0("PC1: ", summary_PCA_tpm_bc[1,1], "%"),
+       y = paste0("PC2: ", summary_PCA_tpm_bc[2,1], "%")) +
+  my_plot_themes
+PCA_BatchCorrected
+# ggsave(PCA_BatchCorrected,
+#        file = paste0("BatchCorrected_TPM_wRv_v2.pdf"),
+#        path = "Figures_preNonCodingRemoval/PCA/wRv",
+#        width = 8, height = 5, units = "in")
+
+# Get the top Gene contributors for the PCA (positive or negative values)
+top_genes_PC2_bc <- as.data.frame(my_PCA_tpm_bc$rotation) %>%
+  rownames_to_column("Gene") %>%
+  arrange(desc(abs(PC2))) %>%
+  slice(1:20) %>%
+  select(Gene, PC2)
+
+
